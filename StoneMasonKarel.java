394d693a1f82ece22889ee7cbb053cb6a99ce50a
S <s>
2013-10-07 08:33:14 -0700

    {"type":"TI", "runs":"2"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -13,95 +13,9 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 
 	public void run(){			
-		orientKarel();
-		moveForward();
-	}
-	
-	// Orients Karel in space
-	private void orientKarel(){
-		// This handles the initial state of Karel
-		if (facingEast() && frontIsClear()){
-			turnLeft();
-		// This handles the situation when Karel gets to the end of the sequence
-		}else if (facingEast() && rightIsBlocked()){
-			stop();
-		}
-	}
-	
-	// Moves Karel forward
-	private void moveForward(){
-		
-		// Set a boolean to detect if this row is a column
-		boolean beepers = false;
-		
-		while(frontIsClear()){
-			if(beepersPresent()){
-				beepers = true;
-			}
-			move();
-		}
-		// At the end of a row, turn Karel around
-		turnAround();
-		
-		// This sets up 2 paths for Karel, depending on if he encounters beepers
-		// First path is for no beepers
-		if(beepers==false){
-			dontLayBeepers();
-		}else{
-			layBeepers();
-		}		
-	}
-	
-	// This functions is for empty rows
-	private void dontLayBeepers(){
-		while(frontIsClear()){
-			move();
-		}
-		restartKarel();
-	}
-	
-	// This function is for rows with beepers
-	private void layBeepers(){
-		// Runs Karel forward
-		while(frontIsClear()){
-			// If beeper present, skip
-			if(beepersPresent()){
 
-			// If beeper not present, lay one down
-			}else if(noBeepersPresent()){
-				putBeeper();
-			}
-			// Moves Karel down one
-			move();
-		}
-		
-		// Handles the end of the row condition
-		if(frontIsBlocked()){
-			if(noBeepersPresent()){
-				putBeeper();
-				restartKarel();
-			}else{
-				// Move Karel to the next row and start the sequence again
-				restartKarel();
-			}
-		}
 	}
 	
-	// This restarts the sequence of Karel going up a row
-	private void restartKarel(){
-		turnLeft();
-		
-		// This checks to see if Karel is at the end row
-		if (facingEast() && frontIsBlocked()){
-			// If so, he stops
-			stop();
-			// Else, he restarts the sequence again
-		}else{
-			move();
-			turnLeft();
-			moveForward();
-		}
-	}
 }
 
 
